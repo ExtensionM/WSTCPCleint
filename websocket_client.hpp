2087e7286fe22ecf06cdc4c12a60a90af05dedaf
sh4869 <nobuk4869@gmail.com>
2015-04-22 02:41:54 +0900

    create websocket_client.hpp

---
+++ websocket_client.hpp
@@ -0,0 +1,36 @@
+#ifndef WSCLIENT_WEBSOCKET_CLIENT_HPP
+#define WSCLIENT_WEBSOCKET_CLIENT_HPP
+
+#include <websocket/config/asio_client.hpp>
+
+#include <websocket/client.hpp>
+
+#include <stirng>
+
+typedef websocketpp::client<websocketpp::config::asio_client> client;
+
+namespace connectserver {
+
+class websocket_client {
+	public:
+		websocket_client(std::string uri);
+		void connect();
+		void start();
+
+		void on_open(websocketpp::connection_hdl hdl);
+		void on_message(websocketpp::connection_hdl hdl);
+		void on_close(websocketpp::connection_hdl hdl);
+		
+		void send(std::string meesage);
+		
+		const std::string get_status();
+	private:
+		client m_client;
+		
+		std::string m_status;
+		std::string m_url;
+};
+
+} //namespace connectserver
+
+#endif 
