c5f920106e841a0bd57d54886a205335b3cf1dcd
sh4869 <nobuk4869@gmail.com>
2015-04-24 21:36:05 +0900

    Create TCP Client Base header

--- src/tcp_client.hpp
+++ src/tcp_client.hpp
@@ -1,1 +1,31 @@
+#ifndef WSCLIENT_TCP_CLIENT_HPP
+#define WSCLIENT_TCP_CLIENT_HPP
 
+#include <boost/asio.hpp>
+
+#include <iostream>
+
+typedef boost::asio::ip::tcp::socket sockect;
+
+namespace connectserver{
+
+class tcp_client {
+	public:
+		tcp_client(std::string ip_address,int port_number);
+		void connect();
+		void send(std::string message);
+	private:
+		boost::asio::io_service m_io_service;
+		socket m_socket;
+		
+		std::string m_address;
+		int m_port_number;
+
+		void on_connect(const boost::system::error_code & error);
+		void on_send(const boost::system::error_code& error,size_t bytes_transferred);
+
+};
+
+}
+
+#endif 
