f01b72b3b06268fbe769e4476f2763f1c587182f
sh4869 <nobuk4869@gmail.com>
2015-04-28 01:44:00 +0900

    fit each function of tcp_client

--- src/tcp_client.cpp
+++ src/tcp_client.cpp
@@ -1,31 +1,45 @@
 #include "tcp_client.hpp"
 
 #include <boost/asio.hpp>
+#include <boost/bind.hpp>
 
 #include <iostream>
 
-typedef boost::asio::ip::tcp::socket sockect;
+typedef boost::asio::ip::tcp::socket socket;
 
 namespace connectserver{
 
 	tcp_client::tcp_client(int port_number){
-	
+		m_port_number = port_number;
+		m_socket = boost::asio::ip::tcp::socket(m_io_service);
 	}
 	
-	tcp_client::connect(){
-	
+	void tcp_client::connect(){
+		m_socket.async_connect(
+				tcp::endpoint(boost::asio::ip::address::from_string("127.0.0.1"),m_port_number),
+				boost::bind(&tcp_client::on_connect,this,asio::placeholders::error));
 	}
 	
-	tcp_client::send(std::string message){
-
+	void tcp_client::send(std::string message){
+		boost::asio::async_write(m_socket,boost::asio::buffer(message),
+				boost::bind(&tcp_client::on_send,this,
+					boost::asio::placeholders::error,
+					boost::asio::placeholders::bytes_transferred));
 	}
 	
-	tcp_client::on_connect(const boost::system::error_code & error){
-
+	void tcp_client::on_connect(const boost::system::error_code & error){
+		if(error){
+			std::cout << "Connect failed : " << error.message() << std::endl;
+			return;
+		}
 	}
 
-	tcp_client::on_send(const boost::system::error_code & error,size_t bytes_readable){
-
+	void tcp_client::on_send(const boost::system::error_code & error,size_t bytes_readable){
+		if(error){
+			std::cout << "send failed : " << error.message() << std::endl;
+		}else{
+			std::cout << "send success" << std::endl;
+		}
 	}
 }
 
