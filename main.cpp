c6ce113563e0d056a10043cc9bc1bd3558e39fed
sh4869 <nobuk4869@gmail.com>
2015-06-28 15:09:10 +0900

    Fix program to open source

--- main.cpp
+++ main.cpp
@@ -2,11 +2,15 @@
 #include "src/tcp_server.hpp"
 
 #include <boost/bind.hpp>
+#include <iostream>
 
 using namespace connectserver;
 
 int main(){
-	server_ras_client *s_client = new server_ras_client("ws://ec2-52-68-77-61.ap-northeast-1.compute.amazonaws.com:3000");
+	std::string url;
+	std::cout << "Please Input URL" << std::endl;
+	cin >> url;
+	server_ras_client *s_client = new server_ras_client(url);
 	s_client->connect();
 	s_client->start();
 }
--- src/websocket_tcp_client.cpp
+++ src/websocket_tcp_client.cpp
@@ -71,18 +71,6 @@ namespace connectserver{
 
 	void server_ras_client::on_open(websocketpp::connection_hdl hdl){
 		m_status = "open";
-		std::ifstream ifs("/home/pi/.setting.json");
-		if(ifs.is_open()){
-			std::string str;
-			ifs >> str;
-			if(str != ""){
-				send(str);
-			}else{
-				send("{\"type\":\"regist\",\"name\":\"sh4869\",\"password\":\"raspitest4869\"}");
-			}
-		}else{
-			send("{\"type\":\"regist\",\"name\":\"sh4869\",\"password\":\"raspitest4869\"}");
-		}
 	}
 
 	void server_ras_client::on_message(websocketpp::connection_hdl hdl,message_ptr msg){
